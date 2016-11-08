1b917560af2cb93012f420bf09e704ae3a0e246a
lincenying <lincenying@qq.com>
2016-11-08 23:48:29 +0800

    fix

--- cooking.conf.js
+++ cooking.conf.js
@@ -4,24 +4,16 @@ var path = require('path');
 var cooking = require('cooking');
 var autoprefixer = require('autoprefixer')
 var browserslist = require('browserslist')
-var webpack = require('webpack');
+var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
-cooking.set({
+var config = {
     entry: {
         app: './src/index.jsx',
         login: './src/login.jsx',
         vendor: [ 'react', 'react-addons-css-transition-group', 'react-dom', 'react-immutable-render-mixin', 'react-router-redux', 'react-redux', 'react-toastr', 'redux', 'redux-form', 'redux-immutablejs', 'immutable', './src/polyfill']
     },
-    dist: './dist',
-    template: [{
-        filename: 'index.html',
-        template: 'src/template/index.html',
-        chunks: ['common', 'vendor', 'app']
-    }, {
-        filename: 'login.html',
-        template: 'src/template/login.html',
-        chunks: ['common', 'vendor', 'login']
-    }],
+    dist: './dist/static',
     externals: {
         'jQuery': 'jquery'
     },
@@ -42,7 +34,7 @@ cooking.set({
     clean: true,
     hash: true,
     sourceMap: false,
-    publicPath: '/',
+    publicPath: '/static/',
     assetsPath: 'images',
     urlLoaderLimit: 10000,
     extractCSS: 'css/[name].[contenthash:7].css',
@@ -50,7 +42,31 @@ cooking.set({
         autoprefixer({ browsers: browserslist('last 2 version, > 0.1%')})
     ],
     extends: ['react', 'eslint', 'less']
-})
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config.template = [{
+        filename: '../index.html',
+        template: 'src/template/index.html',
+        chunks: ['manifest', 'vendor', 'app']
+    }, {
+        filename: '../login.html',
+        template: 'src/template/login.html',
+        chunks: ['manifest', 'vendor', 'login']
+    }]
+} else {
+    config.template = [{
+        filename: 'index.html',
+        template: 'src/template/index.html',
+        chunks: ['vendor', 'app']
+    }, {
+        filename: 'login.html',
+        template: 'src/template/login.html',
+        chunks: ['vendor', 'login']
+    }]
+}
+
+cooking.set(config)
 
 cooking.add('resolve.alias', {
     'src': path.join(__dirname, 'src'),
@@ -64,7 +80,15 @@ if (process.env.NODE_ENV === 'production') {
     cooking.add('output.filename', 'js/[name].[chunkhash].js')
     cooking.add('output.chunkFilename', 'js/[id].[chunkhash].js')
     cooking.add('plugin.CommonsChunk', new webpack.optimize.CommonsChunkPlugin({
-        names: ["common", "vendor"]
+        name: 'vendor',
+        minChunks: function(module, count) {
+            return (module.resource && /\.js$/.test(module.resource) && module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0)
+        }
+    }))
+    cooking.add('plugin.CommonsChunk', new webpack.optimize.CommonsChunkPlugin({name: 'manifest', chunks: ['vendor']}))
+} else {
+    cooking.add('plugin.CommonsChunk', new webpack.optimize.CommonsChunkPlugin({
+        names: ["vendor"]
     }))
 }
 
